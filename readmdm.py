88d50ec18e6824f8fb226023f959c5aa70d399d2
Joshua Gancher <gancherj@reed.edu>
2017-06-01 09:30:32 -0400

    j

--- readmdm.py
+++ readmdm.py
@@ -19,9 +19,16 @@ def read_mdm(filename):
     mdm.structs = structs
     return mdm
 
+def emit_struct(struct, filename, templ, series, namesp, version):
+    return (templ + "-D version=" + version + "-D namespace="+namesp+" -D series="+series+" -D mdm="+filename+" -D struct="+struct.attrib['Name']+"-o "+struct.attrib['Name']+".c")
+
+def emit_struct(enum, filename, templ):
+    return (templ + "-D version=" + version + "-D namespace="+namesp+" -D series="+series+" -D mdm="+filename+" -D enum="+enum.attrib['Name']+"-o "+enum.attrib['Name']+".c")
+
 if __name__ == "__main__":
-    if len(sys.argv) < 2:
-        print("no mdm given")
+    if len(sys.argv) < 4:
+        print("usage: " + sys.argv[0] + " in.mdm struct_templ.c enum_templ.c")
         sys.exit()
     else:
         mdm = read_mdm(sys.argv[1])
+        
