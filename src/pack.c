579cc06bea622fd2b6c5a2e9fe1db7a93889d623
Josh Gancher <jgancher@MacBooks-MacBook-Pro.local>
2017-06-02 09:56:56 -0400

    templ

--- src/pack.c
+++ src/pack.c
@@ -19,6 +19,30 @@ inline void lmcp_pack_char(char c, uint8_t* head) {
     head++;
 }
 
-inline void lmcp_pack_int16(uint16_t i, uint8_t* head) {
-    *(uint16_t
+inline void lmcp_pack_uint16(uint16_t i, uint8_t* head) {
+    uint16_t big_end = i16_htonl(i);
+    uint8_t* data = (*uint8_t) &big_end;
+    head[0] = data[0];
+    head[1] = data[1];
+    head += 2;
+}
+
+inline void lmcp_pack_uint32(uint32_t i, uint8_t* head) {
+
+}
+
+inline void lmcp_pack_string(const char* s, uint8_t* head) {
+    // does not include null termination
+    uint16_t slen = 0; // TODO manually
+    lcmp_pack_uint16(s, head);
+    for (uint16_t i = 0; i < slen; i++)
+        head[i] = ((const uint8_t*)s)[i];
+    head += slen;
+
+}
+
+// todo: pack float, pack signed integer
+
+
+
 
