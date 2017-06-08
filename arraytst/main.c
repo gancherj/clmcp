f52e0eec29c67d4e9d1dcd3e6834976bb422ebeb
Joshua Gancher <gancherj@reed.edu>
2017-06-08 12:12:55 -0400

    this works

--- arraytst/main.c
+++ arraytst/main.c
@@ -1,7 +1,7 @@
 #include "lmcp.h"
 #include <inttypes.h>
 #include <stdio.h>
-TestStruct* g(uint32_t* i, uint32_t size) {
+TestStruct* gstatic(uint32_t* i, uint32_t size) {
     TestStruct *ti;
     lmcp_init_TestStruct(&ti);
 
@@ -21,9 +21,37 @@ TestStruct* g(uint32_t* i, uint32_t size) {
     lmcp_free_TestStruct(ti);
     return to;
 }
+TestStruct* g(uint32_t* i, uint32_t size) {
+    TestStruct *ti;
+    lmcp_init_TestStruct(&ti);
+
+    ti->tstfield = malloc(sizeof(uint32_t) * size);
+    ti->tstfield_ai.length = size;
+    memcpy(ti->tstfield, i, sizeof(uint32_t) * size);
+
+
+    uint8_t* buf = malloc(lmcp_packsize((lmcp_object*)ti));
+    lmcp_pack(buf, (lmcp_object*)ti);
+
+    TestStruct *to;
+    lmcp_unpack(buf, (lmcp_object**)&to);
+
+    free(buf);
+    lmcp_free((lmcp_object*)ti);
+    return to;
+}
 
 uint32_t* f(uint32_t* i, uint32_t size) {
     TestStruct* to = g(i, size);
+    uint32_t* arr = malloc(sizeof(uint32_t) * size);
+    memcpy(arr, to->tstfield, sizeof(uint32_t) * size);
+    lmcp_free((lmcp_object*)to);
+    return arr;
+
+}
+
+uint32_t* fstatic(uint32_t* i, uint32_t size) {
+    TestStruct* to = gstatic(i, size);
     uint32_t* arr = malloc(sizeof(uint32_t) * size);
     memcpy(arr, to->tstfield, sizeof(uint32_t) * size);
     lmcp_free((lmcp_object*)to);
